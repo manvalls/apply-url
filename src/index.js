3271cc687e2ca20e7de7eaead005e3213dc89e57
Manuel Valls Fernández <manolo@vlrz.es>
2020-03-29 20:46:20 +0200

    Export getAbsoluteUrl

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@
 export { default as applyURL } from './applyURL'
 export { default as navigate } from './navigate'
 export { bindPopState } from './urlManager'
+export { default as getAbsoluteUrl } from './getAbsoluteUrl'
